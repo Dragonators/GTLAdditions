d7ecdf14ddf35a041420999b2e4d1d18b1adde99
Dragonators <13391169509@163.com>
2025-08-03 23:33:18 +0800

    AE->任意总成支持超Int流体/物品样板一次性输入
    (cherry picked from commit 9aee6aa41fc93ee05479c7c37bfa79e2477b0cab)

---
+++ src/main/java/com/gtladd/gtladditions/mixin/ae2/ExternalStorageFacadeMixin.java
@@ -0,0 +1,48 @@
+package com.gtladd.gtladditions.mixin.ae2;
+
+import appeng.api.config.Actionable;
+import appeng.api.networking.security.IActionSource;
+import appeng.api.stacks.AEKey;
+import appeng.me.storage.ExternalStorageFacade;
+import com.google.common.primitives.Ints;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(value = ExternalStorageFacade.class)
+public abstract class ExternalStorageFacadeMixin {
+
+    /**
+     * @author Dragons
+     * @reason 流体输入上限
+     */
+    @Overwrite(remap = false)
+    public long insert(AEKey what, long amount, Actionable mode, IActionSource source) {
+        final int max = Integer.MAX_VALUE;
+        int times = Ints.saturatedCast(amount / max);
+        int remainder = (int) (amount % max);
+        long insertedTotal = 0;
+
+        for (; times > 0; times--) {
+            int insert = this.insertExternal(what, max, mode);
+            if (insert == 0) {
+                break;
+            }
+            insertedTotal += insert;
+        }
+
+        if (remainder > 0) {
+            insertedTotal += this.insertExternal(what, remainder, mode);
+        }
+
+        if (insertedTotal > 0 && mode == Actionable.MODULATE) {
+            Runnable changeListener = ((ExternalStorageFacadeAccessor) this).getChangeListener();
+            if (changeListener != null) {
+                changeListener.run();
+            }
+        }
+
+        return insertedTotal;
+    }
+
+    protected abstract int insertExternal(AEKey what, int amount, Actionable mode);
+}
