c9ae244270179851afbd985c2fd070f4965c6748
Dragonators <13391169509@163.com>
2025-08-04 09:37:15 +0800

    有源变压器支持更多输入输出;ME请求器支持15位数值

---
+++ src/main/java/com/gtladd/gtladditions/mixin/meRequester/NumberFieldMixin.java
@@ -0,0 +1,17 @@
+package com.gtladd.gtladditions.mixin.meRequester;
+
+import com.almostreliable.merequester.client.widgets.NumberField;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(NumberField.class)
+public abstract class NumberFieldMixin {
+
+    @ModifyConstant(
+                    method = "<init>",
+                    constant = @Constant(intValue = 7))
+    private int replaceMaxLength(int seven) {
+        return 15;
+    }
+}
