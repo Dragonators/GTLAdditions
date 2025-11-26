9a4faa61490b69db780254f0c71a3479cd488877
Dragonators <13391169509@163.com>
2025-11-27 04:03:41 +0800

    修复ldlib加载@OnlyIn(Dist.CLIENT)导致的崩溃

---
+++ src/main/java/com/gtladd/gtladditions/utils/antichrist/ClientAnimationHelper.kt
@@ -0,0 +1,27 @@
+﻿package com.gtladd.gtladditions.utils.antichrist
+
+import com.github.benmanes.caffeine.cache.Caffeine
+import com.gtladd.gtladditions.common.machine.muiltblock.controller.ForgeOfTheAntichrist
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.api.distmarker.OnlyIn
+
+@OnlyIn(Dist.CLIENT)
+object ClientAnimationHelper {
+    private val ANIMATION_STATES = Caffeine.newBuilder()
+        .weakKeys()
+        .build<ForgeOfTheAntichrist, ClientAnimationState>()
+
+    private fun getOrCreate(machine: ForgeOfTheAntichrist): ClientAnimationState =
+        ANIMATION_STATES.get(machine) { ClientAnimationState() }
+
+    fun onCollapseStateChanged(machine: ForgeOfTheAntichrist, newValue: Boolean, oldValue: Boolean) {
+        getOrCreate(machine).onStateChanged(newValue, oldValue)
+    }
+
+    fun getClientRenderColor(machine: ForgeOfTheAntichrist, baseColor: Int): Int =
+        getOrCreate(machine).getRenderColor(baseColor)
+
+    fun getClientRenderRadius(machine: ForgeOfTheAntichrist, baseRadius: Float): Float =
+        getOrCreate(machine).getRenderRadius(baseRadius)
+
+}
