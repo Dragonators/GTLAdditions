27a419d377c5f7b72d2be999238ed5b1dd3d4273
Dragonators <13391169509@163.com>
2025-11-30 07:53:17 +0800

    无尽输入总成单次导入流体无上限;清理无用代码
    (cherry picked from commit a553d20b1f2f6ce94d1dfe6b60c8b62633c4c70b)

--- src/main/java/com/gtladd/gtladditions/mixin/gtceu/api/recipe/MultiblockStateMixin.java
+++ src/main/java/com/gtladd/gtladditions/mixin/gtceu/api/recipe/MultiblockStateMixin.java
@@ -34,61 +34,52 @@ public abstract class MultiblockStateMixin {
     @Shadow(remap = false)
     public IMultiController lastController;
 
-    @Unique
-    private boolean gTLAdditions$isProcessing = false;
-
     /**
      * @author Dragons
      * @reason Fix ForgeOfTheAntichrist Module <-> ForgeOfTheAntichrist
      */
     @Overwrite(remap = false)
     public void onBlockStateChanged(BlockPos pos, BlockState state) {
-        if (gTLAdditions$isProcessing) return;
-        gTLAdditions$isProcessing = true;
-        try {
-            if (this.world instanceof ServerLevel serverLevel) {
-                if (pos.equals(this.controllerPos)) {
-                    if (this.lastController != null && !state.is(this.lastController.self().getBlockState().getBlock())) {
-                        this.lastController.onStructureInvalid();
-                        MultiblockWorldSavedData mwsd = MultiblockWorldSavedData.getOrCreate(serverLevel);
-                        mwsd.removeMapping((MultiblockState) (Object) this);
-                    }
-                } else {
-                    final var tempThis = (MultiblockState) (Object) this;
-                    final IMultiController controller = tempThis.getController();
-                    if (controller != null) {
-                        final boolean formed = controller.isFormed();
-                        if (formed) {
-                            if (state.getBlock() instanceof ActiveBlock) {
-                                LongSet activeBlocks = tempThis.getMatchContext().getOrDefault("vaBlocks", LongSets.emptySet());
-                                if (activeBlocks.contains(pos.asLong())) {
-                                    return;
-                                }
-                            } else if (serverLevel.getBlockEntity(pos) instanceof IMachineBlockEntity IMBE) {
-                                var metaMachine = IMBE.getMetaMachine();
-                                if (metaMachine instanceof ItemBusPartMachine ||
-                                        metaMachine instanceof FluidHatchPartMachine ||
-                                        metaMachine instanceof HugeBusPartMachine ||
-                                        metaMachine instanceof ForgeOfTheAntichristModuleBase)
-                                    return;
+        if (this.world instanceof ServerLevel serverLevel) {
+            if (pos.equals(this.controllerPos)) {
+                if (this.lastController != null && !state.is(this.lastController.self().getBlockState().getBlock())) {
+                    this.lastController.onStructureInvalid();
+                    MultiblockWorldSavedData mwsd = MultiblockWorldSavedData.getOrCreate(serverLevel);
+                    mwsd.removeMapping((MultiblockState) (Object) this);
+                }
+            } else {
+                final var tempThis = (MultiblockState) (Object) this;
+                final IMultiController controller = tempThis.getController();
+                if (controller != null) {
+                    final boolean formed = controller.isFormed();
+                    if (formed) {
+                        if (state.getBlock() instanceof ActiveBlock) {
+                            LongSet activeBlocks = tempThis.getMatchContext().getOrDefault("vaBlocks", LongSets.emptySet());
+                            if (activeBlocks.contains(pos.asLong())) {
+                                return;
                             }
+                        } else if (serverLevel.getBlockEntity(pos) instanceof IMachineBlockEntity IMBE) {
+                            var metaMachine = IMBE.getMetaMachine();
+                            if (metaMachine instanceof ItemBusPartMachine ||
+                                    metaMachine instanceof FluidHatchPartMachine ||
+                                    metaMachine instanceof HugeBusPartMachine ||
+                                    metaMachine instanceof ForgeOfTheAntichristModuleBase)
+                                return;
                         }
+                    }
 
-                        if (formed && controller.checkPatternWithLock()) {
-                            controller.self().setFlipped(tempThis.isNeededFlip());
-                            controller.onStructureFormed();
-                        } else {
-                            controller.self().setFlipped(false);
-                            controller.onStructureInvalid();
-                            MultiblockWorldSavedData mwsd = MultiblockWorldSavedData.getOrCreate(serverLevel);
-                            mwsd.removeMapping(tempThis);
-                            mwsd.addAsyncLogic(controller);
-                        }
+                    if (formed && controller.checkPatternWithLock()) {
+                        controller.self().setFlipped(tempThis.isNeededFlip());
+                        controller.onStructureFormed();
+                    } else {
+                        controller.self().setFlipped(false);
+                        controller.onStructureInvalid();
+                        MultiblockWorldSavedData mwsd = MultiblockWorldSavedData.getOrCreate(serverLevel);
+                        mwsd.removeMapping(tempThis);
+                        mwsd.addAsyncLogic(controller);
                     }
                 }
             }
-        } finally {
-            gTLAdditions$isProcessing = false;
         }
     }
 }
